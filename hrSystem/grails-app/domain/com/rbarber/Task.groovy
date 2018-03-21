7471938e39bb468d914cf935703765762db369ac
rbarb5 <b6010468@my.shu.ac.uk>
2018-03-21 16:42:25 +0000

    Added Task Controller

--- hrSystem/grails-app/domain/com/rbarber/Task.groovy
+++ hrSystem/grails-app/domain/com/rbarber/Task.groovy
@@ -2,6 +2,14 @@ package com.rbarber
 
 class Task {
 
+String taskName
+Integer numberOfPeople
+String sectionName
+String department
+String timeRequired
+String description
+Boolean taskCompleted
+
     static constraints = {
     }
 }
