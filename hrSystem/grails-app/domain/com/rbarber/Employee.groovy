3a83969314e403cbbd68d1bd5264a30123b6c3a7
rbarb5 <b6010468@my.shu.ac.uk>
2018-03-21 16:01:55 +0000

    Employee Controller Set Up

--- hrSystem/grails-app/domain/com/rbarber/Employee.groovy
+++ hrSystem/grails-app/domain/com/rbarber/Employee.groovy
@@ -2,6 +2,15 @@ package com.rbarber
 
 class Employee {
 
+String fullName
+Date dateOfBirth
+String residence
+Float hourlyRate
+String employeeID
+Date dateEmployed
+String taxCode
+String contract
+
     static constraints = {
     }
 }
